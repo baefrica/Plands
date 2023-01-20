8637b43804a75a6199f41572fe2a94e82545dfb8
김성한 <k0s0a7@naver.com>
2023-01-20 14:49:48 +0900

    [feat] add Rest API for create User

--- Frontend/plands/src/util/api/userApi.js
+++ Frontend/plands/src/util/api/userApi.js
@@ -1,1 +1,9 @@
 import client from "../client";
+
+export const createUser = async (data) => {
+  const response = await client.post(`/beakgu/member`, data);
+
+  console.log(response);
+
+  return response;
+};
