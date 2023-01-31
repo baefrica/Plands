49cd3374f72670687379ddcb03277c9983219dd1
김성한 <k0s0a7@naver.com>
2023-01-31 19:49:51 +0900

    [dirs] remove legacy

--- Frontend/plands/src/pages/HomePage/HomePage.js
+++
@@ -1,5 +0,0 @@
-function HomePage() {
-  return <div>홈페이지 입니다.</div>;
-}
-
-export default HomePage;
--- Frontend/plands/src/util/api/userApi.js
+++
@@ -1,9 +0,0 @@
-import client from "../client";
-
-export const createUser = async (data) => {
-  const response = await client.post(`/beakgu/member`, data);
-
-  console.log(response);
-
-  return response;
-};
