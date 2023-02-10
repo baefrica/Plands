57b61a95181248a06208e8334d7cd8172cb6f051
김성한 <k0s0a7@naver.com>
2023-02-10 17:08:23 +0900

    [remove] backend config legacy version

--- backend/src/main/java/com/ssafy/api/service/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.api.service;
-
-public interface EmailService {
-    public void sendPwd(String email, String pwd) throws Exception;
-    public void emailAuth(String email, String code) throws Exception;
-}
--- backend/src/main/java/com/ssafy/common/db/repository/MemberRepositorySupport.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.common.db.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class MemberRepositorySupport {
-
-
-}
