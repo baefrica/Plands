bdd4f9f2b07e5bdb75c12b935386c86cb82558c0
김성한 <k0s0a7@naver.com>
2023-02-12 02:56:18 +0900

    [design] main page carousel design

--- frontend/src/components/main/carousel/Carousel.style.js
+++ frontend/src/components/main/carousel/Carousel.style.js
@@ -5,10 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import { colors } from "styles/variables";
 
 export const Container = styled.div`
-  position: relative;
-  margin: auto;
-  width: 50%;
-  max-width: 500px;
+  width: 100%;
 `;
 
 export const StyledSlider = styled(Slider)`
@@ -32,7 +29,7 @@ export const ImgContainer = styled.div`
   display: flex;
   position: relative;
   width: 100%;
-  height: 200px;
+  height: 600px;
 
   #carouselContent {
     position: absolute;
@@ -81,12 +78,54 @@ export const Img = styled.img`
   width: 100%;
 `;
 
-const imgUrl1 = require("assets/images/travel1.jpg");
-const imgUrl2 = require("assets/images/travel2.jpg");
+export const CarouselText = styled.div`
+  z-index: 100;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: white;
+`;
+
+export const CarouselTextP = styled.p`
+  font-size: 45px;
+  font-weight: bolder;
+`;
+
+export const CarouselButton = styled.button`
+  border: none;
+  border-radius: 5px;
+  background-color: white;
+  width: 200px;
+  height: 50px;
+  font-size: 20px;
+  font-weight: bolder;
+
+  &:hover {
+    cursor: pointer;
+    color: white;
+    background-color: dimgray;
+  }
+`;
+
+export const ImgFilter = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.12);
+`;
+
 const imgUrl3 = require("assets/images/travel3.jpg");
+const imgUrl4 = require("assets/images/travel4.jpg");
+const imgUrl5 = require("assets/images/travel5.jpg");
+const imgUrl6 = require("assets/images/travel6.jpg");
 
 export const items = [
-  { id: 1, url: imgUrl1 },
-  { id: 2, url: imgUrl2 },
   { id: 3, url: imgUrl3 },
+  { id: 4, url: imgUrl4 },
+  { id: 5, url: imgUrl5 },
+  { id: 6, url: imgUrl6 },
 ];
