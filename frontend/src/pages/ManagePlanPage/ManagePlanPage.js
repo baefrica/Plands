4e4c6014cd13791069c0aaa418c8afe7c4c12e85
김성한 <k0s0a7@naver.com>
2023-02-13 01:06:45 +0900

    [design] manage Plan Page Layout

--- frontend/src/pages/ManagePlanPage/ManagePlanPage.js
+++ frontend/src/pages/ManagePlanPage/ManagePlanPage.js
@@ -3,17 +3,29 @@ import Header from "../../components/header/Header";
 import Nav from "../../components/nav/Nav";
 import AddPlanBtn from "components/plancard/AddPlanBtn";
 import PlanCard from "components/plancard/PlanCard";
-
+import * as S from "./ManagePlanPage.style";
+import axios from "axios";
 const ManagePlanPage = () => {
+  const [planList, setPlanList] = useState(() => {
+    axios.get();
+  });
   const [uuidList, setUuidList] = useState();
 
   return (
-    <div>
+    <>
       <Header />
       <Nav />
-      <PlanCard />
-      <AddPlanBtn />
-    </div>
+      <S.ContentWrapper>
+        <S.PlanListHeader>
+          <S.PlanAddButton>여행계획 추가하기</S.PlanAddButton>
+        </S.PlanListHeader>
+        <S.PlanListWrapper>
+          <PlanCard />
+        </S.PlanListWrapper>
+        <S.PlanListFooter />
+      </S.ContentWrapper>
+      {/*<AddPlanBtn /> */}
+    </>
   );
 };
 
