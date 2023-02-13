2022c709eab3ac5f0c2ade919f73381cde91a93c
김성한 <k0s0a7@naver.com>
2023-02-13 23:51:43 +0900

    [update] plan card component

--- frontend/src/components/plancard/PlanCard.js
+++ frontend/src/components/plancard/PlanCard.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import * as S from "./PlanCard.style";
 
-function PlanCard({ uuid, travelTitle }) {
+function PlanCard({ item }) {
+  const [uuid, setUuid] = useState(item.code);
+  const handlePlancardOnClick = (event) => {
+    // 여기서 모달 띄워서 입장 삭제 버튼 구현
+    // 삭제의 경우에는 leader인 경우에만 표시
+    // 입장 시에는 뒤에 uuid 붙여서 라우팅
+    console.log("-------플랜입니다------", uuid);
+  };
   return (
-    <S.PlanWrapper>
+    <S.PlanWrapper onClick={handlePlancardOnClick}>
       <S.PlanCardHeader></S.PlanCardHeader>
       <S.PlanCardBody></S.PlanCardBody>
       <S.PlanCardFooter>
-        <S.PlanTitle>우주 여행</S.PlanTitle>
+        <S.PlanTitle>{item.title}</S.PlanTitle>
       </S.PlanCardFooter>
     </S.PlanWrapper>
   );
