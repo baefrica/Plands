d0937ded8dfd8abbdc242c19a22179ab8c28f864
김성한 <k0s0a7@naver.com>
2023-02-13 23:51:07 +0900

    [feat] add goplan pages

--- frontend/src/pages/GoPlanPage/GoPlanPage.js
+++ frontend/src/pages/GoPlanPage/GoPlanPage.js
@@ -1,5 +1,44 @@
-const GoPlanPage = () => {
-  return <div>동시편집 페이지</div>;
-};
+import TravelPlanTemplate from "components/collaborative/TravelPlanTemplate";
+import { useState } from "react";
+import VideoSpace from "components/collaborative/VideoSpace";
+import GoPlanHeader from "components/collaborative/GoPlanHeader";
+import * as S from "./GoPlanPage.style";
+import SideNav from "components/collaborative/SideNav";
 
-export default GoPlanPage;
+function App() {
+  const [input, setInput] = useState("");
+  const [room, setRoom] = useState("");
+
+  const handleOnChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleOnClick = () => {
+    setRoom(input);
+  };
+
+  return (
+    <div className="App">
+      <input value={input} onChange={handleOnChange} />
+      <button onClick={handleOnClick}>입장</button>
+
+      {room && (
+        <S.MainContent>
+          <S.StickySpace>
+            <GoPlanHeader />
+            <VideoSpace
+              mySessionId={room}
+              myUserName={"Participant" + Math.floor(Math.random() * 100)}
+            />
+          </S.StickySpace>
+          <S.ContentSpace>
+            <TravelPlanTemplate room={room} />
+            <SideNav />
+          </S.ContentSpace>
+        </S.MainContent>
+      )}
+    </div>
+  );
+}
+
+export default App;
